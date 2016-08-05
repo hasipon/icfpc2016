5c19437e5602b6a17d6c8a5f537eeb6ad8b45aee
Shingo Inada <inada-s@klab.com>
2016-08-06 08:54:34 +0900

    boost multiprecision

--- inada-s/main.cpp
+++ inada-s/main.cpp
@@ -1,25 +1,35 @@
+//#define GV_JS
+//#include "gv.hpp"
+
+#include <boost/assign/list_of.hpp>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <iostream>
-#define GV_JS
-#include "gv.hpp"
 using namespace std;
+using namespace boost::multiprecision;
+
+namespace bg = boost::geometry;
+using point = bg::model::d2::point_xy<cpp_rational>;
+using box = bg::model::box<point>;
+using polygon = bg::model::polygon<point>;
 
 int main() {
-  /*
-for (int i = 0; i < 10; ++i) {
-  gvNewTime();
-  gvOutput(("Hello world" + to_string(i)).c_str());
-  gvLine(0, 0, 100, 100, gvColor(i));
-  gvCircle(i * 10, i * 10, i, gvColor(i));
-}
-   */
-
-  gvMoveTo(0, 0);
-  for (int i = 1; i < 10; ++i) {
-    gvLineTo(g_gvLastLineX + i, g_gvLastLineY + i);
-    gvLineTo(g_gvLastLineX, g_gvLastLineY + i);
-    gvLineTo(g_gvLastLineX - i, g_gvLastLineY);
-    gvArrowTo(g_gvLastLineX, g_gvLastLineY - 1);
+  // box
+  {
+    const box x(point(0, 0), point(3, 3));
+    const auto result = bg::area(x);
+    std::cout << result << std::endl;
   }
-  cout << "Hello" << endl;
-  return 0;
+  // polygon
+  {
+    polygon x;
+    bg::exterior_ring(x) =
+        boost::assign::list_of<point>(0, 0)(0, 3)(3, 3)(0, 0);
+
+    const auto result = bg::area(x);
+    std::cout << result << std::endl;
+  }
 }
