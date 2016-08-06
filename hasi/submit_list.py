cc09e2f92c9c352e05fb63dde7b0d60e9c7c273d
hashimoto-t <hashimoto-t@klab.com>
2016-08-06 21:00:10 +0900

    submit_list.py

---
+++ hasi/submit_list.py
@@ -0,0 +1,10 @@
+import os.path
+from glob import glob
+
+with open(os.path.join(os.path.dirname(__file__), 'solved.txt')) as f:
+    solved = {int(x.split(' ')[0]) for x in f.readlines()}
+
+for x in glob(os.path.join(os.path.dirname(__file__), 'solution_ref', '*.txt')):
+    y = int(os.path.basename(x)[:-4])
+    if y not in solved:
+        print(y)
