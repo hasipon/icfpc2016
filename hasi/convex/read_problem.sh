b9e98693742f4e48612793d847c352568e664268
hasi <takuya.hashimoto@gmail.com>
2016-08-07 18:00:12 +0000

    read_problems

--- hasi/convex/read_problem.sh
+++ hasi/convex/read_problem.sh
@@ -1,8 +1,16 @@
 cd $(dirname $0)
 
+pushd ../../watanabe-yuya/convex/
+make
+popd
+
 for x in ../../problems/*.txt; do
   if [ ! -e problems/$(basename $x) ]; then
     echo $(basename $x)
     python3.5 read_problem.py $x > problems/$(basename $x)
+  fi
+  if [ ! -e hulls/$(basename $x) ]; then
+    echo $(basename $x)
+    ../../watanabe-yuya/convex/a.out < $x > hulls/$(basename $x)
   fi
 done
