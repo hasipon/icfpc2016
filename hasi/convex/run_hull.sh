702d33328877c44f7602a31d80f3e71de81641a9
hasi <takuya.hashimoto@gmail.com>
2016-08-07 18:32:40 +0000

    run_hull.sh

--- hasi/convex/run_hull.sh
+++ hasi/convex/run_hull.sh
@@ -1,6 +1,6 @@
 cd $(dirname $0)
 
-for x in hulls/*.json; do
+for x in hulls/*.txt; do
   if [ ! -e hull_solutions/$(basename $x) ]; then
     echo $x
     python3.5 solve.py $x hull_solutions/$(basename $x)
