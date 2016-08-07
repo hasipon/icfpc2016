e701d53927bd940afa74b7f03aa46a46a5b2c524
hasi <takuya.hashimoto@gmail.com>
2016-08-07 09:48:14 +0000

    convex

--- hasi/convex/run_solve.sh
+++ hasi/convex/run_solve.sh
@@ -3,6 +3,6 @@ cd $(dirname $0)
 for x in problems/*.txt; do
   if [ ! -e solutions/$(basename $x) ]; then
     echo $x
-    echo "python3.5 solve.py $x solutions/$(basename $x)"
+    python3.5 solve.py $x solutions/$(basename $x)
   fi
 done
